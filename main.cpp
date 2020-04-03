4303afffa2417a67f4ab05455cef4592fbbe216c
quartz <vd-larkin@ya.ru>
2020-04-03 13:34:29 +0300

    Change main

--- main.cpp
+++ main.cpp
@@ -3,5 +3,11 @@
 #include <string>
 
 int main() {
-    TGE::ColoredOutput("Hello World\n", TGE::Colors::Magenta, TGE::Colors::White);
+    TGE::ColoredOutput("L", TGE::Colors::White, TGE::Colors::Red);
+    TGE::ColoredOutput("G", TGE::Colors::White, TGE::Colors::Yellow);
+    TGE::ColoredOutput("B", TGE::Colors::White, TGE::Colors::Green);
+    TGE::ColoredOutput("T", TGE::Colors::White, TGE::Colors::Cyan);
+    TGE::ColoredOutput("Q", TGE::Colors::White, TGE::Colors::Blue);
+    TGE::ColoredOutput("+", TGE::Colors::White, TGE::Colors::Magenta);
+    std::cout << '\n';
 }
